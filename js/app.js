6190e7c11df69a416a16d75a556613781011a9fe
darcycervh <darcycervera@gmail.com>
2018-01-29 00:44:53 -0500

    avanzando header

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.dropdown-button').dropdown('open');
+  $('.dropdown-button').dropdown('close');
+  var height = $(window).height();
+  
+  $(document).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var pixels = scrollTop / 70;
+    if(scrollTop < height){
+      $('#forest-background').css({
+        '-webkit-filter':'blur(' + pixels + 'px)',
+        'background-position':'center -' + pixels *10+'px'
+      });
+      $('div-header').fadeOut();
+    }
+  })
+});
